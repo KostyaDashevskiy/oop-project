a8a1344e092bdfe807c32348794627207d91bc2a
Kiriki122 <kiriki496@gmail.com>
2024-06-02 23:00:11 +0300

    add colors from root to pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,24 +16,22 @@ function Header() {
                     </a>
                 </div>
 
-                <div className='header_username'>
-                    <div className='header_menu menu'>
-                        <FaUser className='menu_icon' />
-                        <button className='menu_button' onClick={() => setOpen(!isOpen)}>
-                            <span>USERNAME</span>
-                        </button>
-                        <div className={`header_dropdown ${isOpen ? 'active' : ''}`}>
-                            <ul className='menu_list'>
-                                <li className='menu_item'>
-                                    <FaUser className='icon' />
-                                    <span>Profile</span>
-                                </li>
-                                <li className='menu_item'>
-                                    <BiLogOut className='icon' />
-                                    <span>Log out</span>
-                                </li>
-                            </ul>
-                        </div>
+                <div className='header_menu menu'>
+                    <FaUser className='menu_icon' />
+                    <button className='menu_button' onClick={() => setOpen(!isOpen)}>
+                        USERNAME
+                    </button>
+                    <div className={`header_dropdown ${isOpen ? 'active' : ''}`}>
+                        <ul className='menu_list'>
+                            <li className='menu_item'>
+                                <FaUser className='icon' />
+                                <span>Profile</span>
+                            </li>
+                            <li className='menu_item'>
+                                <BiLogOut className='icon' />
+                                <span>Log out</span>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
--- src/components/Profile/ProfileTabs/BasicInfo.jsx
+++ src/components/Profile/ProfileTabs/BasicInfo.jsx
@@ -1,32 +1,21 @@
-import { FaUser } from 'react-icons/fa6';
-
 function BasicInfo() {
     return (
         <section className='Profile__BasicInfo BasicInfo'>
-            <div className='BasicInfo__UserProfile'>
-                <div className='BasicInfo__UserIcon'>
-                    <FaUser />
-                </div>
-                <div className='BasicInfo__UserInfo'>
-                    <div className='BasicInfo__UserName'>
-                        <div className='BasicInfo__UserName--label'>Username:</div>
-                        <div className='BasicInfo__UserName--data'>placeholder</div>
-                    </div>
-                    <div className='BasicInfo__UserEmail'>
-                        <div className='BasicInfo__UserEmail--label'>Email:</div>
-                        <div className='BasicInfo__UserEmail--data'>placeholder</div>
-                    </div>
-                </div>
+            <div className='BasicInfo__UserName'>
+                <div className='BasicInfo__UserName--label BasicInfo--label'>Username:</div>
+                <div className='BasicInfo__UserName--data BasicInfo--data'>placeholder</div>
             </div>
-            <div className='BasicInfo__games'>
-                <div className='BasicInfo__rating'>
-                    <div className='BasicInfo__rating--label'>Rating:</div>
-                    <div className='BasicInfo__rating--data'>30 000</div>
-                </div>
-                <div className='BasicInfo__wins'>
-                    <div className='BasicInfo__wins--label'>Wins:</div>
-                    <div className='BasicInfo__wins--data'>150</div>
-                </div>
+            <div className='BasicInfo__UserEmail'>
+                <div className='BasicInfo__UserEmail--label BasicInfo--label'>Email:</div>
+                <div className='BasicInfo__UserEmail--data BasicInfo--data'>placeholder</div>
+            </div>
+            <div className='BasicInfo__UserRating'>
+                <div className='BasicInfo__rating--label BasicInfo--label'>Rating:</div>
+                <div className='BasicInfo__rating--data BasicInfo--data'>30 000</div>
+            </div>
+            <div className='BasicInfo__UserWins'>
+                <div className='BasicInfo__wins--label BasicInfo--label'>Wins:</div>
+                <div className='BasicInfo__wins--data BasicInfo--data'>150</div>
             </div>
         </section>
     );
