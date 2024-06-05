d3b63c921af6152e781f17539e268aa1ab5de34f
KostyaDashevskiy <k.dashevskiy22@mail.ru>
2024-06-05 23:04:33 +0300

    Remove react strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,4 @@ import { RouterProvider } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 
-root.render(
-    <React.StrictMode>
-        <RouterProvider router={routes} />
-    </React.StrictMode>,
-);
+root.render(<RouterProvider router={routes} />);
